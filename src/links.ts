cb9d12c92ab07fd260ff8de311ffee9911d91e75
Lewis <mcclelll@icloud.com>
2025-02-15 17:37:42 -0800

    feat: add 404 page

--- src/links.ts
+++ src/links.ts
@@ -4,3 +4,4 @@ export const PHOTO_ALBUM = "https://photos.app.goo.gl/cHvVaUfFe965jwtz6";
 export const GITHUB_ACCOUNT = "https://github.com/FRC3636/frc-2025";
 export const CAD_MODEL =
     "https://cad.onshape.com/documents/91312b208aad8571797edb8e/w/f972da841b2dc7667c8171ec/e/01728691127e205a98aa4858";
+export const CONTACT_US = "mailto:frcteam3636@gmail.com";
